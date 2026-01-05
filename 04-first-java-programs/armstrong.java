8c6d2c1a5aee7d1905e508642c8553d94ffd7f33
Pritisha Kumari <your.pritishakumari.official@gmail.com>
2026-01-05 11:58:18 +0530

    Armstrong no between 2 nos

--- 04-first-java-programs/armstrong.java
+++ 04-first-java-programs/armstrong.java
@@ -1,6 +1,49 @@
-//To find Armstrong Number between two given number.
-
+import java.util.Scanner;
 
 public class armstrong {
-    
+
+    // Method to check Armstrong number
+    static boolean isArmstrong(int num) {
+        int original = num;
+        int sum = 0;
+        int digits = 0;
+        int temp = num;
+
+        // Count digits
+        while (temp > 0) {
+            digits++;
+            temp /= 10;
+        }
+
+        temp = num;
+
+        // Calculate sum of powers of digits
+        while (temp > 0) {
+            int digit = temp % 10;
+            sum += Math.pow(digit, digits);
+            temp /= 10;
+        }
+
+        return sum == original;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter starting number: ");
+        int start = sc.nextInt();
+
+        System.out.print("Enter ending number: ");
+        int end = sc.nextInt();
+
+        System.out.println("Armstrong numbers between " + start + " and " + end + ":");
+
+        for (int i = start; i <= end; i++) {
+            if (isArmstrong(i)) {
+                System.out.print(i + " ");
+            }
+        }
+
+        sc.close();
+    }
 }
